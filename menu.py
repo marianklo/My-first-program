469cdb795703d951cc28a95ef07eddb74ccee2c2
Marian Klonoczki <133518971+marianklo@users.noreply.github.com>
2023-06-09 19:47:19 +0100

    Add files via upload

---
+++ menu.py
@@ -0,0 +1,8 @@
+print("Please order 3 favourite food items from menu")
+item1 = input("Choose your first food :")
+item2 = input("Choose your second food :")
+item3 = input("Choose your third food:")
+print("Order confirmation! You have ordered:")
+print(item1)
+print(item2)
+print(item3)
